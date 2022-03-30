0da509d363c6cf727871c081313283b6690829f9
susan <susanwan@oceanpayment.com.cn>
2022-03-30 16:33:41 +0800

    OXXO

---
+++ app/code/community/Oceanpayment/OPOXXO/Block/Redirect.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magentocommerce.com so we can send you a copy immediately.
+ *
+ * @category	Oceanpayment
+ * @package 	Oceanpayment_OXXO
+ */
+class Oceanpayment_OPOXXO_Block_Redirect extends Mage_Core_Block_Template
+{
+	/**
+	 * Order instance
+	 */
+	protected $_order;
+	
+	/**
+	 *  Get order
+	 *
+	 *  @param    none
+	 *  @return	  Mage_Sales_Model_Order
+	 */
+	public function getOrder()
+	{
+		
+		if ($this->_order == null) {
+			$session = Mage::getSingleton('checkout/session');
+			$this->_order = Mage::getModel('sales/order');
+			$this->_order->loadByIncrementId($session->getLastRealOrderId());
+		}
+		return $this->_order;
+	}
+	
+	/**
+	 * toHtml
+	 *
+	 * @return string
+	 * @deprecated after 1.4.0.1
+	 */
+	protected function _toHtml()
+	{
+		$this->setTemplate('op_oxxo/redirect.phtml');
+
+		return parent::_toHtml();
+	
+	}
+
+	
+	
+	protected function oxxoForm()
+	{
+
+		$standard = Mage::getModel('opoxxo/payment');
+
+        $form = new Varien_Data_Form();
+        $form->setAction($standard->getOXXOUrl())
+            ->setId('oxxo_payment_checkout')
+            ->setName('oxxo_payment_checkout')
+            ->setMethod('POST')
+            ->setUseContainer(true);
+        foreach ($standard->setOrder($this->getOrder())->getStandardCheckoutFormFields() as $field => $value) {
+            $form->addField($field, 'hidden', array('name' => $field, 'value' => $value));
+        }
+
+        $formHTML = $form->toHtml();
+
+        return $formHTML;       
+
+    }
+    
+
+  
+}
